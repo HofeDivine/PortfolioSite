903619445eec7a461624143918a569f647ae387e
HofeDivine <hofedive@gmail.com>
2024-12-03 22:49:48 +0200

    Resetting

--- app/rountes.py
+++ app/rountes.py
@@ -6,14 +6,17 @@ def home():
     return render_template('home.html')
 
 #The about page
-@app.route('/about')
+@app.route('/aboutUs')
 def about():
-    return render_template('about.')
+    return render_template('about-us.html')
 #Projects page
-@app.route('/projects')
-def projects():
-    return render_template('projects.html')
+@app.route('/services')
+def services():
+    return render_template('services.html')
 #Skills page
-@app.route('/skills')
-def skills():
-    return render_template('skills.html')
+@app.route('/portfolio')
+def port():
+    return render_template('portfolio.html')
+@app.route('/hireMe')
+def hireMe():
+    return render_template('hire-me.html')
